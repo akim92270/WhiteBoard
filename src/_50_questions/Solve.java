6f4e4549baa90d60f8eaaade0a0fdec9eceefd64
akim92270 <akim.92270@gmail.com>
2022-04-26 23:18:01 -0500

    added 50 questions

---
+++ src/_50_questions/Solve.java
@@ -0,0 +1,977 @@
+package _50_questions;
+
+public class Solve {
+    /* Question 1
+    Write a method that takes 3 int arguments and returns the greatest number
+    NOTE: 2 or 3 numbers being same is okay
+    TEST DATA:
+    3, 3, 5
+    EXPECTED OUTPUT:
+    5
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 2
+    Write a method that takes 3 int arguments and returns the smallest number
+    NOTE: 2 or 3 numbers being same is okay
+    TEST DATA:
+    1, 7, 13
+    EXPECTED OUTPUT:
+    1
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 3
+    Write a method that takes 3 int arguments and returns the middle number
+    NOTE: 2 or 3 numbers being same is okay
+    TEST DATA:
+    4, 4, 5
+    EXPECTED OUTPUT:
+    4
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 4
+    Write a method that takes a String and returns reversed String
+    TEST DATA:
+    Hello
+    EXPECTED OUTPUT:
+    olleH
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 5
+    Write a method that takes a String and return String back with all digits removed
+    TEST DATA:
+    a1b2c3
+    EXPECTED OUTPUT:
+    abc
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 6
+    Write a method that takes a String and return String back with all spaces removed
+    TEST DATA:
+    Java is fun
+    EXPECTED OUTPUT:
+    Javaisfun
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 7
+    Write a method that takes a String and return String back with all letters removed
+    TEST DATA:
+    Time is 8 PM.
+    EXPECTED OUTPUT:
+    [  8 .]
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 8
+    Write a method that takes a String and return String back with all specials removed
+    TEST DATA:
+    The price = 100.50$
+    EXPECTED OUTPUT:
+    The price  10050
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 9
+    Write a method that takes a String and return String back with all vowels removed
+    TEST DATA:
+    Hello World!
+    EXPECTED OUTPUT:
+    Hll Wrld!
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 10
+    Write a method that takes a String and return String back with all consonants removed
+    TEST DATA:
+    Hello World!
+    EXPECTED OUTPUT:
+    eo o!
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 11
+    Write a method that takes a String and return String back with all uppercase letters removed
+    TEST DATA:
+    Hello World!
+    EXPECTED OUTPUT:
+    ello orld!
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 12
+    Write a method that takes a String and return String back with all uppercase letters removed
+    TEST DATA:
+    Hello World!
+    EXPECTED OUTPUT:
+    H W!
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 13
+    Write a method that takes a positive int argument and prints all the numbers from 1 to given number
+    TEST DATA:
+    5
+    EXPECTED OUTPUT:
+    1
+    2
+    3
+    4
+    5
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 14
+    Write a method that takes a positive int argument and prints all the even numbers from 1 to given number
+    TEST DATA:
+    10
+    EXPECTED OUTPUT:
+    2
+    4
+    6
+    8
+    10
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 15
+    Write a method that takes a positive int argument and prints all the odd numbers from 1 to given number
+    TEST DATA:
+    9
+    EXPECTED OUTPUT:
+    1
+    3
+    5
+    7
+    9
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 16
+    Write a method that takes a positive int argument and prints all the numbers from 1 to given number
+    However, it prints “Fizz” for the numbers divided by 3, “Buzz” for the numbers divided by 5,
+    and “FizzBuzz” for the numbers divided by both 3 and 5
+    TEST DATA:
+    15
+    EXPECTED OUTPUT:
+    1
+    2
+    Fizz
+    4
+    Buzz
+    Fizz
+    7
+    8
+    Fizz
+    Buzz
+    11
+    Fizz
+    13
+    14
+    FizzBuzz
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 17
+    Write a method that takes an int array argument and returns the sum of all int elements
+    TEST DATA:
+    [0, -5, 3, 5, 4]
+    EXPECTED OUTPUT:
+    7
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 18
+    Write a method that takes an int array argument and returns the product of all int elements
+    TEST DATA:
+    [3, 5, 4]
+    EXPECTED OUTPUT:
+    60
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 19
+    Write a method that takes an int array argument and returns the average of all int elements
+    TEST DATA:
+    [3, 5, 4, 0, -2]
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 20
+    Write a method that takes an int array argument and returns the count of zeros
+    TEST DATA:
+    [3, 5, 4, 0, 0]
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 21
+    Write a method that takes an int array argument and returns the count of positive numbers
+    TEST DATA:
+    [3, 5, 4, 0, -2]
+    EXPECTED OUTPUT:
+    3
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 22
+    Write a method that takes an int array argument and returns the count of negative numbers
+    TEST DATA:
+    [-3, 5, 4, 0, -2]
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 23
+    Write a method that takes an int array argument and returns the count of numbers greater than 10
+    TEST DATA:
+    [-3, 15, 44, 0, -2]
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 24
+    Write a method that takes an int array argument and returns the count of prime numbers
+    NOTE: Prime number is a number that can be divided only by 1 and itself
+    NOTE: Negative numbers cannot be prime
+    NOTE: The smallest prime number is 2
+    Examples: 2,3,5,7,11,13,17,19,23,29,31,37 etc.
+    TEST DATA:
+    [-3, 5, 4, 0, 11]
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 25
+    Write a method that takes an int array argument and returns the count of even numbers
+    TEST DATA:
+    [-3, 5, 4, 0, -2]
+    EXPECTED OUTPUT:
+    3
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 26
+    Write a method that takes an int array argument and returns the count of odd numbers
+    TEST DATA:
+    [-3, 5, 4, 0, -2]
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 27
+    Write a method that takes an int array argument and returns the array back with all duplicates removed
+    NOTE: the order of elements returned is not an issue.
+    TEST DATA:
+    [0, 5, 4, 0, 0]
+    EXPECTED OUTPUT:
+    [0, 4, 5]
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 28
+    Write a method that takes a String array argument and returns the array back with all duplicates removed
+    NOTE: the order of elements returned is not an issue.
+    TEST DATA:
+    ["foo", "bar", "foo", "123", "bar"]
+    EXPECTED OUTPUT:
+    [bar, 123, foo]
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 29
+    Write a method that takes a String argument and returns a boolean. It will return true if given String is palindrome, and false otherwise
+    Palindrome: it is a word that reads the same backward as forward
+    Ex: kayak, civic, madam
+    TEST DATA:
+    Hello
+    EXPECTED OUTPUT:
+    false
+    TEST DATA:
+    civic
+    EXPECTED OUTPUT:
+    true
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 30
+    Write a method that takes a String argument and returns a boolean. It will return true if given String is palindrome, and false otherwise
+    Anagram: a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+    Ex: listen-silent | cinema-iceman
+    TEST DATA:
+    Hello, World
+    EXPECTED OUTPUT:
+    false
+    TEST DATA:
+    cinema, iceman
+    EXPECTED OUTPUT:
+    true
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 31
+    Write a method that takes an Integer ArrayList argument and returns the list back with all duplicates removed
+    NOTE: the order of elements returned is not an issue.
+    TEST DATA:
+    [0, 5, 4, 0, 0]
+    EXPECTED OUTPUT:
+    [0, 4, 5]
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 32
+    Write a method that takes a String ArrayList argument and returns the list back with all duplicates removed
+    NOTE: the order of elements returned is not an issue.
+    TEST DATA:
+    ["foo", "bar", "foo", "123", "bar"]
+    EXPECTED OUTPUT:
+    [bar, 123, foo]
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 33
+    Write method that takes an int argument and prints the Fibonacci series of given number
+    DEFINITION: Fibonacci is a series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers.
+    The simplest is the series 0, 1, 1, 2, 3, 5, 8, 13, 21
+    TEST DATA:
+    8
+    EXPECTED OUTPUT:
+    0
+    1
+    1
+    2
+    3
+    5
+    8
+    13
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 34
+    Write a method that takes a String argument and returns the count of spaces as an int
+    TEST DATA:
+    Java is fun
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 35
+    Write a method that takes a String argument and returns the count of letters as an int
+    TEST DATA:
+    Hello World!
+    EXPECTED OUTPUT:
+    10
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 36
+    Write a method that takes a String argument and returns the count of digits as an int
+    TEST DATA:
+    Happy 2022!
+    EXPECTED OUTPUT:
+    4
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 37
+    Write a method that takes a String argument and returns the count of specials as an int
+    TEST DATA:
+    The date was 10/10/1990
+    EXPECTED OUTPUT:
+    2
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 38
+    Write a method that takes a String argument and returns the count of vowels as an int
+    TEST DATA:
+    I like Java
+    EXPECTED OUTPUT:
+    5
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 39
+    Write a method that takes a String argument and returns the count of consonants as an int
+    TEST DATA:
+    I like Java
+    EXPECTED OUTPUT:
+    6
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 40
+    Write a method that takes a String argument and returns the count of upper cases as an int
+    TEST DATA:
+    Java is fun
+    EXPECTED OUTPUT:
+    1
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 41
+    Write a method that takes a String argument and returns the count of lower cases as an int
+    TEST DATA:
+    Java is fun
+    EXPECTED OUTPUT:
+    8
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 42
+    Write a method that takes a String as argument and returns the count of words as an int
+    TEST DATA:
+    Java is fun
+    EXPECTED OUTPUT:
+    3
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 43
+    Write a method that takes a String as argument and returns the String back with all extra spaces removed
+    TEST DATA:
+    Java    is    fun
+    EXPECTED OUTPUT:
+    Java is fun
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 44
+    Write a method that takes a String as argument and returns the String back with each word reversed in the same place
+    TEST DATA:
+    Java is fun
+    EXPECTED OUTPUT:
+    avaJ si nuf
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 45
+    Write a method that takes an int argument and returns number’s factorial.
+    Factorial is the operation of multiplying any natural number with all the natural numbers that are smaller than it
+    Ex: 4! = 4*3*2*1, so the result is 24
+    NOTE: 0! = 1
+    NOTE: Negative numbers are not considered in this method
+    TEST DATA:
+    5
+    EXPECTED OUTPUT:
+    120
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 46
+    Write a method that takes an int array argument and returns the array back with all elements replaced with their factorial.
+    Factorial is the operation of multiplying any natural number with all the natural numbers that are smaller than it
+    Ex: 4! = 4*3*2*1, so the result is 24
+    NOTE: 0! = 1
+    NOTE: Negative numbers are not considered in this method
+    TEST DATA:
+    [0, 5, 4, 1, 3, 2]
+    EXPECTED OUTPUT:
+    [1, 120, 24, 1, 6, 2]
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 47
+    Write a method that takes an int array argument and returns the max value as an int
+    NOTE: Assume length of the array is always more than zero
+    TEST DATA:
+    [-5, 4, 1, 10, 2]
+    EXPECTED OUTPUT:
+    10
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 48
+    Write a method that takes an int array argument and returns the min value as an int
+    NOTE: Assume length of the array is always more than zero
+    TEST DATA:
+    [-5, 4, 1, 10, 2]
+    EXPECTED OUTPUT:
+    -5
+     */
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 49
+    Write a method that takes an int array argument and returns the second max value as an int
+    NOTE: Assume length of the array is always more than one
+    TEST DATA:
+    [-5, 4, 1, 10, 2]
+    EXPECTED OUTPUT:
+    4
+     */
+
+
+
+
+
+
+
+
+
+
+
+
+    /* Question 50
+    Write a method that takes an int array argument and returns the second min value as an int
+    NOTE: Assume length of the array is always more than one
+    TEST DATA:
+    [-5, 4, 1, 10, 2]
+    EXPECTED OUTPUT:
+    1
+     */
+
+
+
+
+
+
+
+
+
+
+
+}
